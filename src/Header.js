9f62c4618a46a90c613393866a7fb34c4ccf9cb3
Hari Nair <nairgh@hotmail.com>
2024-07-15 22:58:07 -0400

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Button,
+  Menu,
+  MenuItem,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import AddBusinessRoundedIcon from "@mui/icons-material/AddBusinessRounded";
+import DrawerComp from "./Drawer";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+const Header = () => {
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  console.log(theme);
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(isMatch);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+  return (
+    <React.Fragment>
+      <AppBar sx={{ background: "#063970" }} >
+        <Toolbar>
+          <AddBusinessRoundedIcon sx={{ transform: "scale(2)" }} />
+          {isMatch ? (
+            <>
+              <Typography sx={{ fontSize: "2rem", paddingLeft: "10%" }}>
+                Shoppee
+              </Typography>
+              <DrawerComp />
+            </>
+          ) : (
+            <>
+              <Tabs
+                sx={{ marginLeft: "auto" }}
+                indicatorColor="secondary"
+                textColor="inherit"
+                value={value}
+                onChange={(e, value) => setValue(value)}
+              >
+                <Tab label="Products" />
+                <Tab label="Services" />
+                <Tab label="About Us" />
+                <Tab label="Contact" />
+              </Tabs>
+              <Button
+                color="inherit"
+                id="resources-button"
+                aria-controls={open ? "resources-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                endIcon={<KeyboardArrowDownIcon />}
+                onClick={handleClick}
+              >
+                Resources
+              </Button>
+              <Menu
+                id="resources-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                MenuListProps={{
+                  "aria-labelledby": "resources-button",
+                }}
+              >
+                <MenuItem onClick={handleClose}>Blog</MenuItem>
+                <MenuItem onClick={handleClose}>Podcast</MenuItem>
+              </Menu>
+              <Button sx={{ marginLeft: "auto" }} variant="contained">
+                Login
+              </Button>
+              <Button sx={{ marginLeft: "10px" }} variant="contained">
+                SignUp
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Header;
